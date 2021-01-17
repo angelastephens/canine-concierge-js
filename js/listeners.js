2ad6d6a2de1d3fa2defbe4f75fba0fd27cf40213
angela stephens <angelastephens317@gmail.com>
2021-01-17 00:30:46 -0500

    add modal functions

--- js/listeners.js
+++ js/listeners.js
@@ -2,13 +2,23 @@ document.addEventListener("click", (e) => {
     let target = e.target
     if(target.matches("#myDoggos")) {
        Dog.get_dogs()
-    }}
+    } else if(target.matches("#addDog")) {
+        Modal.populate({title: "Add New Dog", content: "New Dog"})
+        Modal.toggle()    
+    }
+    else if(target.matches(".modal-close") || target.matches(".modal-overlay")) {
+        e.preventDefault();
+        Modal.toggle();
+      } 
+})
+
     
-) 
+ 
 
 document.addEventListener("DOMContentLoaded", (e) => {
     console.log()
     Dog.get_dogs().then(() => ServiceRequest.get_service_requests())
+    Modal.init();
     
 })
 
@@ -17,7 +27,21 @@ document.addEventListener("submit", (e) => {
     if(target.matches("#newService")) {
         e.preventDefault()
     ServiceRequest.post_service_requests()
-    }}
-    
-) 
+    }  if(target.matches("#letsGo")) {
+        Dog.create()
+    }
+})
 
+
+document.addEventListener('keydown', function(evt) {
+  evt = evt || window.event
+  var isEscape = false
+  if ("key" in evt) {
+    isEscape = (evt.key === "Escape" || evt.key === "Esc")
+  } else {
+    isEscape = (evt.keyCode === 27)
+  }
+  if (isEscape && document.body.classList.contains('modal-active')) {
+    Modal.toggle()
+  }
+});
