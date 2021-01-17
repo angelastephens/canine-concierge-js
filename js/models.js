f1b2d7faca8bdf11303eacb57d8151f862c8b7d1
angela stephens <angelastephens317@gmail.com>
2021-01-17 00:31:02 -0500

    add static create

--- js/models.js
+++ js/models.js
@@ -53,6 +53,36 @@ class Dog{
         document.getElementById("dog_choices").appendChild(this.option)
 
     }
+
+    static create(formData) {
+        return fetch("http://localhost:3000/dogs", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({dog: formData})
+        })
+            .then(res => {
+                if(res.ok) {
+                    return res.json()
+                }   else {
+                    return res.text().then(error => Promise.reject(error)) 
+                }
+            })
+            .then(dogAttributes => {
+                let dog = new Dog(dogAttributes);
+                this.get_dogs.push(dog);
+                this.collection().appendChild(dog.display());
+                new FlashMessage({type: 'WoofWoof!', message: "New pup added successfully"})
+                return dog;
+            }) 
+            .catch(error => {
+                new FlashMessage({type: 'error', message: error});
+            })
+    }
+
+    
 }
 
 class ServiceRequest{
@@ -137,7 +167,29 @@ class ServiceRequest{
             })
         
         }
+
+    
        
 
 }
 
+class Modal {
+    static init() {
+      this.body ||= document.body;
+      this.modal ||= document.querySelector('.modal');
+      this.title ||= document.querySelector('#modal-title');
+      this.content ||= document.querySelector('#modal-content');
+    }
+  
+    static populate({title, content}) {
+      this.title.innerText = title;
+      this.content.innerHTML = ""; 
+      this.content.append(content);
+    }
+  
+    static toggle() {
+      this.modal.classList.toggle('opacity-0');
+      this.modal.classList.toggle('pointer-events-none');
+      this.body.classList.toggle('modal-active');
+    }
+}
